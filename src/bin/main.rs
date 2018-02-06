ff2c7d1051cadd6b09cb67709ffd9a2be30c6a9e
Sven Strittmatter <ich@weltraumschaf.de>
2018-02-06 13:21:09 +0100

    Add CLI options

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -2,20 +2,82 @@ extern crate hello;
 #[macro_use]
 extern crate log;
 extern crate simple_logger;
+extern crate clap;
 
 use std::io::prelude::*;
 use std::net::TcpListener;
 use std::net::TcpStream;
 use std::fs::File;
-use hello::ThreadPool;
-
+use clap::{Arg, App};
+use hello::Config;
+use hello::threads::ThreadPool;
 
 fn main() {
     simple_logger::init().unwrap();
     info!("Starting web server ...");
 
-    let listener = TcpListener::bind("127.0.0.1:8080").unwrap();
-    let pool = ThreadPool::new(4);
+    let matches = App::new("Minimalistic HTTP Server")
+        .version("1.0.0")
+        .author("Sven Strittmatter <ich@weltraumschaf.de>")
+        .about("A minimalistic HTTP server.")
+        .arg(Arg::with_name("address")
+            .short("a")
+            .long("address")
+            .takes_value(true)
+            .help(format!("The IP address to bind to. Default is {}.", hello::DEFAULT_ADDRESS).as_str()))
+        .arg(Arg::with_name("port")
+            .short("p")
+            .long("port")
+            .takes_value(true)
+            .help(format!("The port to bind to. Default is {}.", hello::DEFAULT_PORT).as_str()))
+        .arg(Arg::with_name("threads")
+            .short("t")
+            .long("threads")
+            .takes_value(true)
+            .help(format!("Number of parallel threads used to serve. Default is {}", hello::DEFAULT_NUMBER_OF_THREADS).as_str()))
+        .arg(Arg::with_name("dir")
+            .short("d")
+            .long("dir")
+            .takes_value(true)
+            .required(true)
+            .help("The directory with the files to serve."))
+        .get_matches();
+
+    let address = matches.value_of("address")
+        .unwrap_or(hello::DEFAULT_ADDRESS);
+    let port = matches.value_of("port")
+        .unwrap_or(hello::DEFAULT_PORT)
+        .parse::<u16>()
+        .unwrap();
+
+    if port < 1 {
+        panic!("Port must be grater than 0!");
+    }
+
+    let number_of_threads = matches.value_of("threads")
+        .unwrap_or(hello::DEFAULT_NUMBER_OF_THREADS)
+        .parse::<usize>()
+        .unwrap();
+
+    if number_of_threads < 1 {
+        panic!("Number of threads must be grater than 0!");
+    }
+
+    let dir = matches.value_of("dir").unwrap();
+
+    let config = Config::new(address, port, number_of_threads, dir);
+
+    bind_listener(config);
+
+}
+
+fn bind_listener(config: Config) {
+    let addr = format!("{}:{}", config.address, config.port);
+    info!("Bind to {}", addr);
+    let listener = TcpListener::bind(addr).unwrap();
+
+    info!("Serving with {} threads.", config.threads);
+    let pool = ThreadPool::new(config.threads);
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
