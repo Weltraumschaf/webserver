eb7e007a5b8cf65b1a886021237fe3c9ed43ee09
Sven Strittmatter <ich@weltraumschaf.de>
2018-02-07 09:28:26 +0100

    Extract methods to server

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -4,14 +4,10 @@ extern crate log;
 extern crate simple_logger;
 extern crate clap;
 
-use std::io::prelude::*;
-use std::net::TcpListener;
-use std::net::TcpStream;
-use std::fs::File;
 use clap::{Arg, App};
 use hello::Config;
-use hello::threads::ThreadPool;
 use hello::server;
+use hello::server::Server;
 
 fn main() {
     simple_logger::init().unwrap();
@@ -74,45 +70,7 @@ fn main() {
 
     let config = Config::new(address, port, number_of_threads, dir);
 
-    bind_listener(config);
+    let server = Server::new(config);
+    server.bind();
 }
 
-fn bind_listener(config: Config) {
-    let addr = format!("{}:{}", config.address, config.port);
-    info!("Bind to {}", addr);
-    let listener = TcpListener::bind(addr).unwrap();
-
-    info!("Serving with {} threads.", config.threads);
-    let pool = ThreadPool::new(config.threads);
-
-    for stream in listener.incoming() {
-        let stream = stream.unwrap();
-
-        pool.execute(|| {
-            handle_connection(stream);
-        });
-    }
-}
-
-fn handle_connection(mut stream: TcpStream) {
-    let mut buffer = [0; 512];
-    stream.read(&mut buffer).unwrap();
-
-    let get = b"GET / HTTP/1.1\r\n";
-
-    let (status_line, filename) = if buffer.starts_with(get) {
-        ("HTTP/1.1 200 OK\r\n\r\n", "hello.html")
-    } else {
-        ("HTTP/1.1 404 NOT FOUND\r\n\r\n", "404.html")
-    };
-
-    let mut file = File::open(filename).unwrap();
-    let mut contents = String::new();
-
-    file.read_to_string(&mut contents).unwrap();
-
-    let response = format!("{}{}", status_line, contents);
-
-    stream.write(response.as_bytes()).unwrap();
-    stream.flush().unwrap();
-}
--- src/server.rs
+++ src/server.rs
@@ -1,3 +1,61 @@
+use std::io::prelude::*;
+use std::net::TcpListener;
+use std::net::TcpStream;
+use std::fs::File;
+use Config;
+use threads::ThreadPool;
+
 pub const DEFAULT_ADDRESS: &str = "127.0.0.1";
 pub const DEFAULT_PORT: &str = "8080";
 pub const DEFAULT_NUMBER_OF_THREADS: &str = "4";
+
+pub struct Server<'a> {
+    config: Config<'a>,
+}
+
+impl<'a> Server<'a> {
+    pub fn new(config: Config) -> Server {
+        Server { config }
+    }
+
+    pub fn bind(self) {
+        let addr = format!("{}:{}", self.config.address, self.config.port);
+        info!("Bind to {}", addr);
+        let listener = TcpListener::bind(addr).unwrap();
+
+        info!("Serving with {} threads.", self.config.threads);
+        let pool = ThreadPool::new(self.config.threads);
+
+        for stream in listener.incoming() {
+            let stream = stream.unwrap();
+
+            pool.execute(|| {
+                // FIXME
+//                self.handle_connection(stream);
+            });
+        }
+    }
+
+    fn handle_connection(self, mut stream: TcpStream) {
+        let mut buffer = [0; 512];
+        stream.read(&mut buffer).unwrap();
+
+        let get = b"GET / HTTP/1.1\r\n";
+
+        let (status_line, filename) = if buffer.starts_with(get) {
+            ("HTTP/1.1 200 OK\r\n\r\n", "hello.html")
+        } else {
+            ("HTTP/1.1 404 NOT FOUND\r\n\r\n", "404.html")
+        };
+
+        let mut file = File::open(filename).unwrap();
+        let mut contents = String::new();
+
+        file.read_to_string(&mut contents).unwrap();
+
+        let response = format!("{}{}", status_line, contents);
+
+        stream.write(response.as_bytes()).unwrap();
+        stream.flush().unwrap();
+    }
+}
